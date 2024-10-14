df5e2238c2872157ff0898fcd44f5e8363550502
JacksonPessoaS <jacksonpessoas@gmail.com>
2024-10-13 22:13:11 -0300

    Inicial

--- lideres.js
+++
@@ -1,8 +0,0 @@
-function getLideres() {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Liderança');
-    var data = sheet.getRange(2, 1, sheet.getLastRow() - 1, 1).getValues();
-    var lideres = data.map(function(row) {
-        return row[0];
-    });
-    return ContentService.createTextOutput(JSON.stringify(lideres)).setMimeType(ContentService.MimeType.JSON);
-}
--- registrar_presenca.js
+++
@@ -1,7 +0,0 @@
-function registrarPresenca(e) {
-    var data = JSON.parse(e.postData.contents);
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Presencas');
-    sheet.appendRow([data.nome, new Date()]);
-    
-    return ContentService.createTextOutput('Presença registrada com sucesso!');
-}
